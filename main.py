61179a17e4726355b221c1c179d5080351f96a11
haro3250 <147556959+haro3250@users.noreply.github.com>
2023-10-22 21:17:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,21 +13,9 @@ def actions(node, G, A, E):
 def result(node, action):
     return transition_model(node, action)
 
-# Define the step cost
-def step_cost(node, action, new_node):
-    return normalized(T_traffic(node, new_node) + distance(node, new_node))
-
-# Define the heuristic function (if needed)
-def heuristic(node, goal):
-    return distance(node, goal)  
-
 # Define the transition model
 def transition_model(node, action):
     return action  
-
-# Define the goal test
-def goal_test(node, destination):
-    return node == destination
 
 # Define the path cost
 def path_cost(path, G):
